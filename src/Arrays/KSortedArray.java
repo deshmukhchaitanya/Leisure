c1b1932625a90de0e7bd354a11184ff507619e8b
deshmukhchaitanya <chaitanya_deshmukh@yahoo.com>
2015-02-07 08:19:50 -0700

    Boggle BruteForce

---
+++ src/Arrays/KSortedArray.java
@@ -0,0 +1,45 @@
+package Arrays;
+
+/* Given an array in which an element is at most k distance from its actual 
+ * place(in sorted array). Write a program to sort the array. expected in O(nlogk)
+ * Idea is to use a min heap of size k. we know that an element at index i in sorted
+ * array would be the smallest element in the min heap considering we have sorted 
+ * the i-1 elements which can be removed from heap easily in logk time. we repeat 
+ * this process for n elements giving complexity O(nlogk).
+ */ 
+
+import java.util.PriorityQueue;
+
+public class KSortedArray 
+{
+	public static void main(String[] args) 
+	{
+		int k = 3;
+	    int arr[] = {2, 6, 3, 12, 56, 8};
+	    sortKSortedArray(arr,k);
+	    for(int i:arr)
+	    	System.out.print(i+" ");
+	}
+	private static void sortKSortedArray(int[] arr,int k) 
+	{
+		PriorityQueue<Integer> minheap=new PriorityQueue<Integer>(k+1);
+		int n=arr.length-1;
+		// fill k elements
+		for(int i=0;i<=k;i++)
+			minheap.add(arr[i]);
+		int i;
+		// iterate over the array and keep removing from the heap  
+		for(i=0;i<n-k;i++)
+		{
+			arr[i]=minheap.poll();
+			minheap.offer(arr[i+k+1]);
+		}
+		// remove the remaining k elements
+		while(minheap.peek()!=null)
+		{
+			arr[i]=minheap.poll();
+			i++;
+		}
+	}
+
+}
---
+++ src/Matrix/BoggleMatrix.java
@@ -0,0 +1,70 @@
+package Matrix;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+
+public class BoggleMatrix {
+
+	public static void main(String[] args) {
+		char mat[][]={{'S','M','E','F'},
+				      {'R','A','T','D'},
+				      {'L','O','N','I'},
+				      {'K','U','C','B'}};
+		HashSet<String> words=boggle(mat);
+		TreeSet<String> sortedWords=new TreeSet<String>();
+		sortedWords.addAll(words);
+		for(String str: sortedWords)
+		{
+			System.out.println(str);
+		}
+	}
+
+	private static HashSet<String> boggle(char[][] mat) 
+	{
+		HashSet<String> totalWords=new HashSet<String>();
+		for (int i = 0; i < mat.length; i++) 
+		{
+			for (int j = 0; j < mat[0].length; j++) 
+			{
+				HashSet<Character> visited=new HashSet<Character>();
+				HashSet<String> words=boggleRecursive(mat,i,j,visited);
+				totalWords.addAll(words);
+			}
+		}
+		return totalWords;
+	}
+
+	private static HashSet<String> boggleRecursive(char[][] mat, int i, int j, HashSet<Character> visited) 
+	{
+		HashSet<String> totalwords=new HashSet<String>();
+		visited.add(mat[i][j]);
+		totalwords.add(mat[i][j]+"");
+		if(visited.size()>=mat.length*mat[0].length)
+		{
+			totalwords.add(mat[i][j]+"");
+			return totalwords;
+		}
+		int xMin=(i==0)?0:i-1;
+		int xMax=(i==mat.length-1)?mat.length-1:i+1;
+		int yMin=(j==0)?0:j-1;
+		int yMax=(j==mat[0].length-1)?mat[0].length-1:j+1;
+		for(int k=xMin;k<=xMax;k++)
+		{
+			for(int l=yMin;l<=yMax;l++)
+			{
+				if(!(k==i && l==j) && !visited.contains(mat[k][l]))
+				{
+					HashSet<String> l1=boggleRecursive(mat, k, l, visited);
+					for(String str:l1)
+					{
+						totalwords.add(str);
+						if(str.charAt(0)==mat[k][l])
+							totalwords.add(mat[i][j]+str);
+					}
+					visited.remove(mat[k][l]);
+				}	
+			}
+		}
+		return totalwords;
+	}
+}
